6a0fbdb7dd0da26155f140e045a5f90204fdd094
AcaUrosevic <aleksandar.urosevic@fonis.rs>
2025-10-03 19:43:10 +0200

    initial commit

---
+++ server/src/main/java/rc/ac/fon/gymtracker/ServerApplication.java
@@ -0,0 +1,13 @@
+package rc.ac.fon.gymtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
